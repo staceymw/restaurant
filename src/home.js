129f1d35031f3131178fc986aa0dea30f7b9fd37
staceymw <116839832+staceymw@users.noreply.github.com>
2023-06-01 11:59:36 -0400

    Create business info, and chef info section to home

--- src/home.js
+++ src/home.js
@@ -1,5 +1,6 @@
 import MainRestaurant from "./imgs/main-restaurant.jpg";
-import { createDivider, createParagraph } from "./utility";
+import Chef from "./imgs/chef.jpeg";
+import { createDivider, createParagraph, createContainer, createH2 } from "./utility";
 
 function createHome() {
     const home = document.createElement('div');
@@ -21,13 +22,58 @@ function createHome() {
 
     const restaurantInfo = "Come in to Beiko's Bakery, where passion and flavor unite to create mouthwatering delights that will satisfy your sweet cravings. We specialize in crafting extraordinary cakes and delectable pastries that are as visually stunning as they are delicious. Our skilled bakers blend traditional recipes with innovative techniques, resulting in creations that are both nostalgic and contemporary. From elegantly designed wedding cakes to whimsical birthday treats, each confection is meticulously handcrafted with the finest ingredients and an abundance of love. Step into our bakery and indulge in a world of irresistible flavors and captivating aromas that will transport you to a realm of sweet enchantment.";
 
+    const businessInfoContainer = document.createElement("div");
+    businessInfoContainer.classList.add("business-info");
+
+    const hoursHeading = document.createElement("h2");
+    hoursHeading.classList.add("hours-heading");
+    hoursHeading.textContent = "Business Hours";
+
+    const hoursWeekday = "Mon-Wed 8am-10pm";
+    const hoursThur = "CLOSED";
+    const hoursWeekend = "Sat-Sun 7am-11pm";
+
+    const locationHeading = "Location";
+    const locationClass = "location-heading";
+    const locationInfo = "3355 Oakville Place";
+    const locationInfo2 = "Anywhere Valley. NH";
+    const locationInfo3 = "00000";
+
+    const chefImgContainer = document.createElement("div");
+    chefImgContainer.classList.add("img-container");
+     
+    const chefHeading = "Chef Beiko Fasso";
+
+    const chefImg = document.createElement("img");
+    chefImg.classList.add("img");
+    chefImg.src = Chef;
+    chefImg.setAttribute("id", "chef-img");
+    
     restaurantImgContainer.appendChild(mainRestaurantImg);
     home.appendChild(restaurantImgContainer);
     home.appendChild(createDivider());
+
     restaurantInfoContainer.appendChild(restaurantInfoHeading);
     restaurantInfoContainer.appendChild(createParagraph(restaurantInfo));
     home.appendChild(restaurantInfoContainer);
     home.appendChild(createDivider());
+
+    businessInfoContainer.appendChild(hoursHeading);
+    businessInfoContainer.appendChild(createParagraph(hoursWeekday));
+    businessInfoContainer.appendChild(createParagraph(hoursThur));
+    businessInfoContainer.appendChild(createParagraph(hoursWeekday));
+    businessInfoContainer.appendChild(createParagraph(hoursWeekend));
+    businessInfoContainer.appendChild(createH2(locationHeading, locationClass));
+    businessInfoContainer.appendChild(createParagraph(locationInfo));
+    businessInfoContainer.appendChild(createParagraph(locationInfo2));
+    businessInfoContainer.appendChild(createParagraph(locationInfo3));
+    home.appendChild(businessInfoContainer);
+    home.appendChild(createDivider());
+
+    chefImgContainer.appendChild(createH2(chefHeading));
+    chefImgContainer.appendChild(chefImg);
+    home.appendChild(chefImgContainer);
+    
 
 
     return home;
