9f56c69f36412cda76c6baf6fb78580f51003fa1
staceymw <116839832+staceymw@users.noreply.github.com>
2023-06-12 14:00:43 -0400

    Delete comments

--- src/index.js
+++ src/index.js
@@ -3,19 +3,3 @@ import initializeWebsite from "./website";
 import "./style.css";
 
 initializeWebsite();
-/*function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
-  
-    element.innerHTML = _.join(['Hello', 'restaurant'], ' ');
-
-    btn.innerHTML = "Click Me";
-    btn.onclick = printMe;
-    
-    element.appendChild(btn);
-    
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());*/
