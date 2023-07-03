d074e3161e9894c7e7e01aa0b376530b0d1ca53d
staceymw <116839832+staceymw@users.noreply.github.com>
2023-07-03 11:39:52 -0400

    Prepare for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,10 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
-  entry: {
-    index: './src/index.js',
-    website: './src/website.js',
-    home: './src/home.js',
-    menu: './src/menu.js',
-    contact: './src/contact.js',
-    utility: './src/utility'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-  ],
+  entry: "./src/index.js",
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  optimization: {
-    runtimeChunk: 'single',
   },
 };
